27dd7df9215958acf99e581db39cc45ec4b16c53
sujith-hub <gurrapusujith@gmail.com>
2022-12-26 09:42:32 +0530

    Create config.h

---
+++ config.h
@@ -0,0 +1,26 @@
+#define IO_USERNAME "sujio"
+#define IO_KEY "aio_SYmx61azm8haZywPaxmg17eIbpWJ"
+
+#define WIFI_SSID "IOT"
+#define WIFI_PASS "Srix@123"
+
+// uncomment the following line if you are using airlift
+// #define USE_AIRLIFT
+#include "AdafruitIO_WiFi.h"
+
+#if defined(USE_AIRLIFT) || defined(ADAFRUIT_METRO_M4_AIRLIFT_LITE) ||         \
+    defined(ADAFRUIT_PYPORTAL)
+// Configure the pins used for the ESP32 connection
+#if !defined(SPIWIFI_SS) // if the wifi definition isnt in the board variant
+// Don't change the names of these #define's! they match the variant ones
+#define SPIWIFI SPI
+#define SPIWIFI_SS 10 // Chip select pin
+#define NINA_ACK 9    // a.k.a BUSY or READY pin
+#define NINA_RESETN 6 // Reset pin
+#define NINA_GPIO0 -1 // Not connected
+#endif
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS, SPIWIFI_SS,
+                   NINA_ACK, NINA_RESETN, NINA_GPIO0, &SPIWIFI);
+#else
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+#endif
